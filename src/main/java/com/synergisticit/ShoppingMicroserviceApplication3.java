f00d37e9ca2a7c183543031ab78293fef1d2ce27
David Hwang <hwangdav000@gmail.com>
2024-10-27 23:54:14 -0500

    updated microservice

--- src/main/java/com/synergisticit/ShoppingMicroserviceApplication3.java
+++ src/main/java/com/synergisticit/ShoppingMicroserviceApplication3.java
@@ -7,10 +7,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 
-public class ShoppingMicroserviceApplication {
+public class ShoppingMicroserviceApplication3 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShoppingMicroserviceApplication.class, args);
+		SpringApplication.run(ShoppingMicroserviceApplication3.class, args);
 	}
 
 }
